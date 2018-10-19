0aa6aeb60ceb547c401737d781c2da9680b40be3
Ben Schmidt <ben@noodlecake.com>
2018-10-19 15:00:18 -0600

    Initial project import. All ad networks integrated and lots of build errors worked through. Now hitting dex limit.

---
+++ Assets/Scripts/AdsManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Heyzap;
+
+public class AdsManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        // Your Publisher ID is: 7e4482c1be37901e95ec6b42c8e14e21
+        HeyzapAds.Start("7e4482c1be37901e95ec6b42c8e14e21", HeyzapAds.FLAG_NO_OPTIONS);
+
+		HeyzapAds.ShowMediationTestSuite();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
