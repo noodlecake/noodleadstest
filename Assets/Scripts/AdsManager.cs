bed9c91d1e6a016d01a147fde23053946347364f
Ben Schmidt <ben@noodlecake.com>
2018-10-23 16:31:35 -0600

    Add buttons to call ads from outside of the test suite

--- Assets/Scripts/AdsManager.cs
+++ Assets/Scripts/AdsManager.cs
@@ -5,6 +5,8 @@ using Heyzap;
 
 public class AdsManager : MonoBehaviour {
 
+	bool bannerShowing;
+
 	// Use this for initialization
 	void Start () {
         // Your Publisher ID is: 7e4482c1be37901e95ec6b42c8e14e21
@@ -12,6 +14,11 @@ public class AdsManager : MonoBehaviour {
 
 		HeyzapAds.ShowDebugLogs();
 		HeyzapAds.ShowThirdPartyDebugLogs();
+
+		HZVideoAd.Fetch();
+		HZIncentivizedAd.Fetch();
+
+		bannerShowing = false;
 	}
 	
 	// Update is called once per frame
@@ -21,5 +28,34 @@ public class AdsManager : MonoBehaviour {
 
 	public void OpenTestSuite() {
 		HeyzapAds.ShowMediationTestSuite();
+	}
+
+	public void ShowInterstitial() {
+		HZInterstitialAd.Show();
+	}
+
+	public void ShowVideo() {
+		if (HZVideoAd.IsAvailable()) {
+    		HZVideoAd.Show();
+		}
+	}
+
+	public void ShowRewarded() {
+		if (HZIncentivizedAd.IsAvailable()) {
+            HZIncentivizedAd.Show();
+        }
+	}
+
+	public void ToggleBanner() {
+		if (!bannerShowing) {
+        	HZBannerShowOptions showOptions = new HZBannerShowOptions();
+        	showOptions.Position = HZBannerShowOptions.POSITION_BOTTOM;
+        	HZBannerAd.ShowWithOptions(showOptions);
+			bannerShowing = true;
+		} else {
+        	HZBannerAd.Hide();
+			HZBannerAd.Destroy();
+			bannerShowing = false;
+		}
 	}
 }
