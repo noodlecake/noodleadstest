4457040d344ea479aeefcc990eb9df9ab652da15
Ben Schmidt <ben@noodlecake.com>
2018-12-10 16:38:13 -0600

    IronSource imported and initializing

--- Assets/Scripts/AdsManager.cs
+++ Assets/Scripts/AdsManager.cs
@@ -1,22 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Heyzap;
+
 
 public class AdsManager : MonoBehaviour {
+
+#if UNITY_IOS
+	const string IRONSOURCE_KEY = "7e2f58f5";
+#elif UNITY_ANDROID
+	const string IRONSOURCE_KEY = "7e2f927d";
+#endif
+
 
 	bool bannerShowing;
 
 	// Use this for initialization
 	void Start () {
-        // Your Publisher ID is: 7e4482c1be37901e95ec6b42c8e14e21
-        HeyzapAds.Start("7e4482c1be37901e95ec6b42c8e14e21", HeyzapAds.FLAG_NO_OPTIONS);
 
-		HeyzapAds.ShowDebugLogs();
-		HeyzapAds.ShowThirdPartyDebugLogs();
-
-		HZVideoAd.Fetch();
-		HZIncentivizedAd.Fetch();
+		IronSource.Agent.init (IRONSOURCE_KEY);
+		IronSource.Agent.validateIntegration();
 
 		bannerShowing = false;
 	}
@@ -26,35 +28,30 @@ public class AdsManager : MonoBehaviour {
 		
 	}
 
+	void OnApplicationPause(bool isPaused) {                 
+  		IronSource.Agent.onApplicationPause(isPaused);
+	}
+
 	public void OpenTestSuite() {
-		HeyzapAds.ShowMediationTestSuite();
+
 	}
 
 	public void ShowInterstitial() {
-		HZInterstitialAd.Show();
+
 	}
 
 	public void ShowVideo() {
-		if (HZVideoAd.IsAvailable()) {
-    		HZVideoAd.Show();
-		}
+
 	}
 
 	public void ShowRewarded() {
-		if (HZIncentivizedAd.IsAvailable()) {
-            HZIncentivizedAd.Show();
-        }
+
 	}
 
 	public void ToggleBanner() {
 		if (!bannerShowing) {
-        	HZBannerShowOptions showOptions = new HZBannerShowOptions();
-        	showOptions.Position = HZBannerShowOptions.POSITION_BOTTOM;
-        	HZBannerAd.ShowWithOptions(showOptions);
 			bannerShowing = true;
 		} else {
-        	HZBannerAd.Hide();
-			HZBannerAd.Destroy();
 			bannerShowing = false;
 		}
 	}
