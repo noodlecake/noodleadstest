972cd4738d827b24f981828cf264388d06b00f39
Ben Schmidt <ben@noodlecake.com>
2019-01-07 16:04:58 -0600

    Blank slate again

--- Assets/Scripts/AdsManager.cs
+++ Assets/Scripts/AdsManager.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Heyzap;
 
 public class AdsManager : MonoBehaviour {
 
@@ -9,14 +8,6 @@ public class AdsManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        // Your Publisher ID is: 7e4482c1be37901e95ec6b42c8e14e21
-        HeyzapAds.Start("7e4482c1be37901e95ec6b42c8e14e21", HeyzapAds.FLAG_NO_OPTIONS);
-
-		HeyzapAds.ShowDebugLogs();
-		HeyzapAds.ShowThirdPartyDebugLogs();
-
-		HZVideoAd.Fetch();
-		HZIncentivizedAd.Fetch();
 
 		bannerShowing = false;
 	}
@@ -27,35 +18,22 @@ public class AdsManager : MonoBehaviour {
 	}
 
 	public void OpenTestSuite() {
-		HeyzapAds.ShowMediationTestSuite();
+
 	}
 
 	public void ShowInterstitial() {
-		HZInterstitialAd.Show();
+
 	}
 
 	public void ShowVideo() {
-		if (HZVideoAd.IsAvailable()) {
-    		HZVideoAd.Show();
-		}
+
 	}
 
 	public void ShowRewarded() {
-		if (HZIncentivizedAd.IsAvailable()) {
-            HZIncentivizedAd.Show();
-        }
+
 	}
 
 	public void ToggleBanner() {
-		if (!bannerShowing) {
-        	HZBannerShowOptions showOptions = new HZBannerShowOptions();
-        	showOptions.Position = HZBannerShowOptions.POSITION_BOTTOM;
-        	HZBannerAd.ShowWithOptions(showOptions);
-			bannerShowing = true;
-		} else {
-        	HZBannerAd.Hide();
-			HZBannerAd.Destroy();
-			bannerShowing = false;
-		}
+
 	}
 }
